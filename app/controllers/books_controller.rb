e7036eb970afe541cce6399a15d7593f85376ab8
Tim Hoover <timothyhoov@gmail.com>
2013-08-25 12:38:18 -0400

    Add description validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,6 +48,6 @@ class BooksController < ApplicationController
 	private
 
 	def book_params
-		params.require(:book).permit(:title, :author, :year, :rating)
+		params.require(:book).permit(:title, :author, :year, :rating, :description)
 	end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-	validates_presence_of :title, :author, :year, :rating
+	validates_presence_of :title, :author, :year, :rating, :description
 	validates :rating, numericality: {only_integer: true}
 	validates :year, numericality: {only_integer: true}
 
